6dd16d407ddeb81eb08aaa6662345cb090d30947
Ben Thomasson <ben.thomasson@gmail.com>
2018-09-06 15:38:14 -0400

    Add postgres check to django entrypoint

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 # This entrypoint is used to play nicely with the current cookiecutter configuration.
 # Since docker-compose relies heavily on environment variables itself for configuration, we'd have to define multiple
@@ -14,5 +14,31 @@ fi
 export DATABASE_URL=postgres://$POSTGRES_ENV_POSTGRES_USER:$POSTGRES_ENV_POSTGRES_PASSWORD@postgres:5432/$POSTGRES_ENV_POSTGRES_USER
 
 export CELERY_BROKER_URL=$REDIS_URL
+
+postgres_ready() {
+python << END
+import sys
+
+import psycopg2
+
+try:
+    psycopg2.connect(
+        dbname="${POSTGRES_ENV_POSTGRES_DB}",
+        user="${POSTGRES_ENV_POSTGRES_USER}",
+        password="${POSTGRES_ENV_POSTGRES_PASSWORD}",
+        host="postgres",
+        port="5432",
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+
+END
+}
+until postgres_ready; do
+  >&2 echo 'Waiting for PostgreSQL to become available...'
+  sleep 1
+done
+>&2 echo 'PostgreSQL is available'
 
 exec "$@"
